c01858c9229751ab2a928963196999e24ecf4ae4
HGRon <higordasilvalins@gmail.com>
2020-09-24 17:18:35 -0300

    Commit referente a entraga do dia 24

--- TooNear-Solution/src/sample/Main.java
+++ TooNear-Solution/src/sample/Main.java
@@ -6,16 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-//#C4C4C4
-//#8A25B1
-
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../fxml/Base.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1366, 768));
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/Principal.fxml"));
+        primaryStage.setTitle("TOO|NEAR-Solutions");
+        primaryStage.setScene(new Scene(root, 1366, 700));
         primaryStage.show();
     }
 
